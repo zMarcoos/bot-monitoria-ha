77764753e7bff3f1717c5b177064cd9cd4a3fc25
zMarcoos <marcosvdcdef@gmail.com>
2024-11-23 01:45:30 -0300

    Implementação da aprovação por reações

--- src/commands/user.js
+++ src/commands/user.js
@@ -1,10 +1,10 @@
-import { SlashCommandBuilder, AttachmentBuilder } from 'discord.js';
-import { createCanvas, loadImage } from 'canvas';
-import { createEmbed } from '../utils/messageUtils.js';
-import { fileURLToPath } from 'url';
-import path from 'path';
-import UserService from '../database/services/userService.js';
-import { EMBED_COLORS } from '../utils/constants.js';
+import { SlashCommandBuilder, AttachmentBuilder } from "discord.js";
+import { createCanvas, loadImage } from "canvas";
+import { createEmbed } from "../utils/messageUtils.js";
+import { fileURLToPath } from "url";
+import path from "path";
+import UserService from "../database/services/userService.js";
+import { EMBED_COLORS } from "../utils/constants.js";
 
 const JAKE_REGIONS = [
   { xMin: 60, yMin: 140, xMax: 182, yMax: 260 },
@@ -15,7 +15,10 @@ const JAKE_REGIONS = [
 ];
 
 const FINN_REGIONS = [
-  { xMin: 0, yMin: 0, xMax: 0, yMax: 0 },
+  { xMin: 50, yMin: 160, xMax: 93, yMax: 240 },
+  { xMin: 95, yMin: 190, xMax: 270, yMax: 270 },
+  { xMin: 300, yMin: 220, xMax: 335, yMax: 320 },
+  { xMin: 210, yMin: 140, xMax: 270, yMax: 190 },
 ];
 
 function drawStar(context, x, y, radius, color) {
@@ -30,8 +33,14 @@ function drawStar(context, x, y, radius, color) {
     const angleOuter = (index * 2 * Math.PI) / spikes - Math.PI / 2;
     const angleInner = ((index + 0.5) * 2 * Math.PI) / spikes - Math.PI / 2;
 
-    context.lineTo(x + Math.cos(angleOuter) * outerRadius, y + Math.sin(angleOuter) * outerRadius);
-    context.lineTo(x + Math.cos(angleInner) * innerRadius, y + Math.sin(angleInner) * innerRadius);
+    context.lineTo(
+      x + Math.cos(angleOuter) * outerRadius,
+      y + Math.sin(angleOuter) * outerRadius,
+    );
+    context.lineTo(
+      x + Math.cos(angleInner) * innerRadius,
+      y + Math.sin(angleInner) * innerRadius,
+    );
   }
 
   context.closePath();
@@ -40,27 +49,34 @@ function drawStar(context, x, y, radius, color) {
 }
 
 function drawRegionBoxes(context, regions) {
-  context.strokeStyle = 'red';
+  context.strokeStyle = "red";
   context.lineWidth = 2;
 
-  regions.forEach(region => {
-    context.strokeRect(region.xMin, region.yMin, region.xMax - region.xMin, region.yMax - region.yMin);
+  regions.forEach((region) => {
+    context.strokeRect(
+      region.xMin,
+      region.yMin,
+      region.xMax - region.xMin,
+      region.yMax - region.yMin,
+    );
   });
 }
 
 async function generateImageWithStars(character, starCount = 1) {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  const background = await loadImage(path.join(__dirname, `../../assets/${character}.png`));
+  const background = await loadImage(
+    path.join(__dirname, `../../assets/${character}.png`),
+  );
 
   const canvas = createCanvas(background.width, background.height);
-  const context = canvas.getContext('2d');
+  const context = canvas.getContext("2d");
 
   context.drawImage(background, 0, 0, canvas.width, canvas.height);
 
   for (let index = 0; index < starCount; index++) {
-    const regions = character === 'Finn' ? FINN_REGIONS : JAKE_REGIONS;
-
+    const regions = character === "Finn" ? FINN_REGIONS : JAKE_REGIONS;
+    drawRegionBoxes(context, regions);
     let randomRegion = regions[Math.floor(Math.random() * regions.length)];
 
     const usedPositions = new Set();
@@ -70,8 +86,14 @@ async function generateImageWithStars(character, starCount = 1) {
     const maxAttempts = 10;
 
     do {
-      x = Math.floor(Math.random() * (randomRegion.xMax - randomRegion.xMin) + randomRegion.xMin);
-      y = Math.floor(Math.random() * (randomRegion.yMax - randomRegion.yMin) + randomRegion.yMin);
+      x = Math.floor(
+        Math.random() * (randomRegion.xMax - randomRegion.xMin) +
+          randomRegion.xMin,
+      );
+      y = Math.floor(
+        Math.random() * (randomRegion.yMax - randomRegion.yMin) +
+          randomRegion.yMin,
+      );
 
       const positionKey = `${x}-${y}`;
 
@@ -86,64 +108,69 @@ async function generateImageWithStars(character, starCount = 1) {
         randomRegion = regions[Math.floor(Math.random() * regions.length)];
         attempts = 0;
       }
-
     } while (true);
 
     const starSize = 10;
-    drawStar(context, x, y, starSize, 'yellow');
+    drawStar(context, x, y, starSize, "yellow");
   }
 
-  return canvas.toBuffer('image/png');
+  return canvas.toBuffer("image/png");
 }
 
 export default {
   cooldown: 5,
   data: new SlashCommandBuilder()
-    .setName('usuario')
-    .setDescription('Comando de usuários')
-    .addStringOption(option =>
-      option.setName('acao')
-        .setDescription('Categoria de ação')
+    .setName("usuario")
+    .setDescription("Comando de usuários")
+    .addStringOption((option) =>
+      option
+        .setName("acao")
+        .setDescription("Categoria de ação")
         .setRequired(true)
-        .addChoices(
-          { name: 'Avatar', value: 'avatar' },
-        )),
+        .addChoices({ name: "Avatar", value: "avatar" }),
+    ),
   async execute(interaction) {
-    const action = interaction.options.getString('acao');
+    const acao = interaction.options.getString("acao");
 
-    switch (action) {
-      case 'avatar':
+    switch (acao) {
+      case "avatar":
         const userService = new UserService();
         const user = await userService.getUser(interaction.user.id);
         if (!user) {
           await interaction.reply({
-            content: 'Você não está registrado no sistema.',
+            content: "Você não está registrado no sistema.",
             embeds: [
               createEmbed({
-                title: 'Registro',
-                description: 'Você não está registrado. Entre novamente no servidor!',
+                title: "Registro",
+                description:
+                  "Você não está registrado. Entre novamente no servidor!",
                 color: EMBED_COLORS.RED,
-              })
+              }),
             ],
-            ephemeral: true
+            ephemeral: true,
           });
           return;
         }
 
-        const imageBuffer = await generateImageWithStars(user.character, user.activityHistory.length);
-        const attachment = new AttachmentBuilder(imageBuffer, { name: 'avatar.png' });
+        const imageBuffer = await generateImageWithStars(
+          user.character,
+          user.activityHistory.length,
+        );
+        const attachment = new AttachmentBuilder(imageBuffer, {
+          name: "avatar.png",
+        });
         await interaction.reply({ files: [attachment] });
         break;
       default:
         await interaction.reply({
           embeds: [
             createEmbed({
-              title: 'Usuário',
-              description: 'Ação não encontrada.',
+              title: "Usuário",
+              description: "Ação não encontrada.",
               color: EMBED_COLORS.RED,
-            })
+            }),
           ],
-          ephemeral: true
+          ephemeral: true,
         });
         break;
     }
